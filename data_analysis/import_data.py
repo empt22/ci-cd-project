63357f2aa2720882545d3380f3027aa17b0fef4e
Elizabeth <Elizabeth.Lakata@gmail.com>
2025-01-17 16:38:41 +0000

    fixed commas

--- data_analysis/import_data.py
+++ data_analysis/import_data.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-data = {'car': ['A','B','C','D'], 'Price': [20000,21000,20500,23000]}
+data = {'car': ['A', 'B', 'C', 'D'], 'Price': [20000, 21000, 20500, 23000]}
 
 df = pd.DataFrame(data)
