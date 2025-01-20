b3f06d22074f540cff59a723e4804a474a4a2ec6
empt22 <Elizabeth.Lakata@gmail.com>
2025-01-20 19:01:36 +0000

    add test if model loads

--- tests/test_model_loading.py
+++ tests/test_model_loading.py
@@ -1,9 +1,8 @@
 import pickle
-import pytest
+
 
 # to confirm model exists and can be loaded
 def test_model_loading():
     with open('linear_model.pkl', 'rb') as f:
         model = pickle.load(f)
     assert model is not None
-
